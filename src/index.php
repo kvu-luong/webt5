fb6576116d63854a67ae29efa4acdffa02fa3b56
kvu-luong <luongkhanhvu1392@gmail.com>
2020-05-04 11:31:24 +0700

    html inital

---
+++ src/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Web SoSanhGiaNhanh</title>
+		<link href="./css/style.css" rel="stylesheet"></link>
+	</head>
+	<body>
+		<div class="grid-37">
+			<div id="left-side">
+				<div class="logo">
+					<img alt="logo" src="./assets/img">
+				</div>
+				<div class="navigation">
+					<nav>
+						<ul>
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">Online Book</a></li>
+							<li><a href="#">Shop</a></li>
+							<li><a href="#">About Us</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="footer">
+					<h5>Where to find and buy fastest and cheapest on the wolrd.<strong>AVU design</strong></h5>
+				</div>
+			</div>
+			<div id="right-side">
+				<div class="header">
+					<div>
+						<input type="text" placeholder="Search Your Product Here!!!">
+					</div>
+					<div>
+						<nav class="category">
+							<ul>
+								<li><a href="#">All</a></li>
+								<li><a href="#">Skin-care</a></li>
+								<li><a href="#">Books</a></li>
+								<li><a href="#">Courses</a></li>
+								<li><a href="#">Health</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+
+				<div class="content">
+					<div class="content_block">
+						<div><h4>Hot Products</h4></div>
+						<div>
+							<div class="product">
+							</div>
+						</div>
+					</div>
+					<div class="content_block">
+						<div><h4>All Products</h4></div>
+						<div>
+							<div class="product">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<body>
+<html>
